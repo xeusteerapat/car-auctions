41b4e4978c23e1c79971c52b4c900969c05dd120
xeusteerapat <xeus085@gmail.com>
2023-08-08 21:24:53 +0700

    :sparkles: [Update] update delete auction endpoint

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -79,4 +79,21 @@ public class AuctionsController : ControllerBase
 
     return BadRequest("Problem saving change");
   }
+
+  [HttpDelete("{id}")]
+  public async Task<ActionResult> DeleteAuction(Guid id)
+  {
+    var auction = await _context.Auctions.FindAsync(id);
+
+    if (auction == null) return NotFound();
+
+    //TODO: check seller == username
+    _context.Auctions.Remove(auction);
+
+    var result = await _context.SaveChangesAsync() > 0;
+
+    if (!result) return BadRequest("Cannot update change to the DB");
+
+    return Ok();
+  }
 }
