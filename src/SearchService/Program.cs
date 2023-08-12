dd2bd9f1b96a9439b1377f2d05959805dcdd7192
xeusteerapat <xeus085@gmail.com>
2023-08-13 00:42:21 +0700

    :sparkles: [Upadte] update retry mechanism

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -19,6 +19,13 @@ builder.Services.AddMassTransit(x =>
 
     x.UsingRabbitMq((context, cfg) =>
       {
+        cfg.ReceiveEndpoint("search-auction-created", e =>
+        {
+          e.UseMessageRetry(retry => retry.Interval(5, 5));
+
+          e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
+
         cfg.ConfigureEndpoints(context);
       }
     );
