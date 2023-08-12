3d8fcfeb4e421f2b2e70f8ed11ce10f8d6fe4cff
xeusteerapat <xeus085@gmail.com>
2023-08-12 14:27:39 +0700

    :sparkles: [Update] update http polling

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -17,13 +17,22 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-  await DbInitializer.InitDb(app);
-}
-catch (Exception e)
-{
-  Console.WriteLine(e);
-}
+  try
+  {
+    await DbInitializer.InitDb(app);
+  }
+  catch (Exception e)
+  {
+    Console.WriteLine(e);
+  }
+});
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+  => HttpPolicyExtensions
+      .HandleTransientHttpError()
+      .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+      .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
