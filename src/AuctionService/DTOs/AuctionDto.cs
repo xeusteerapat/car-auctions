af56287d57d17fc80fde5e4adbf992ffccfe7d7f
xeusteerapat <xeus085@gmail.com>
2023-08-13 17:53:58 +0700

    :pencil2: [Fix] fix typo

--- src/AuctionService/DTOs/AuctionDto.cs
+++ src/AuctionService/DTOs/AuctionDto.cs
@@ -6,7 +6,7 @@ public class AuctionDto
   public int ReservePrice { get; set; }
   public string Seller { get; set; }
   public string Winner { get; set; }
-  public int SoleAmount { get; set; }
+  public int SoldAmount { get; set; }
   public int CurrentHighBid { get; set; }
   public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
   public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
--- src/SearchService/Models/Item.cs
+++ src/SearchService/Models/Item.cs
@@ -7,7 +7,7 @@ public class Item : Entity
   public int ReservePrice { get; set; }
   public string Seller { get; set; }
   public string Winner { get; set; }
-  public int SoleAmount { get; set; }
+  public int SoldAmount { get; set; }
   public int CurrentHighBid { get; set; }
   public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
   public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
